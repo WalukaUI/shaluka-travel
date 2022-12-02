3862974ceb999ceb6f95fdb1b78455c61da13c56
WalukaUI <cwaluka@yahoo.com>
2022-12-02 16:39:09 -0600

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react"
+import NavBar from'./NavBar/NavBar.js'
+
+
+function Home() {
+    return <>
+    <NavBar/>
+    <h1>This is home</h1>
+    </>
+}
+
+export default Home;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+import './NavBar.css'
+function NavBar() {
+    return<>
+    <div className="NavBar">
+        <div>Home</div>
+        <div>Packages</div>
+        <div>Destinations</div>
+        <div>Conatct me</div>
+    </div>
+    </>
+}
+export default NavBar;
